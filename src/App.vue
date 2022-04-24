f77930ff87aeefbddd9eb217b1d759a778fe4301
nicodwi_k <nicodwika@gmail.com>
2022-04-24 19:41:22 +0700

    change name

--- src/App.vue
+++ src/App.vue
@@ -11,17 +11,11 @@
         height="170"
         gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
       >
-        <v-avatar size="70" class="mb-2">
-          <img
-            src="https://s.gravatar.com/avatar/ce7f3697e231df38b3ca6065848520da?s=160"
-            alt="Danny Connell"
-          >
+        <v-avatar size="70" class="mb-2" color="black">
+          <span class="white--text">NK</span>
         </v-avatar>
         <div class="white--text text-subtitle-1 font-weight-bold">
-          Danny Connell
-        </div>
-        <div class="white--text text-subtitle-2">
-          danny__connell
+          Nico Dwi Kuswanto
         </div>
       </v-img>
 
